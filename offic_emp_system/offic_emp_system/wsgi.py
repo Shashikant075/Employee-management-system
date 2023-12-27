8666da534003946ba359e12f1d6c0cc1d3efea03
Shashikanttt <shashikantt075@gmail.com>
2023-12-27 17:10:54 +0530

    up

--- offic_emp_system/offic_emp_system/wsgi.py
+++ offic_emp_system/offic_emp_system/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'offic_emp_system.settings')
 
 application = get_wsgi_application()
+app =application
